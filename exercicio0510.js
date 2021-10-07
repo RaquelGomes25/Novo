6f9b3a4156a9aa700cd6325c57d624d1d9efa27b
unknown <kelmaria25@gmail.com>
2021-10-07 12:20:26 -0300

    testando

---
+++ exercicio0510.js
@@ -0,0 +1,67 @@
+debugger
+aluno1 = prompt("Qual o nome do aluno 1 ?")
+notas1 = prompt(("Digite três notas separadas por virgula:"))
+notas1 = notas1.split(",")
+n1 =notas1[0]
+n2 =notas1[1]
+n3 =notas1[2]
+media1 = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3
+if ( media1 >= 5 ){
+    alert ("Aprovado" + aluno1)
+}
+else if ( media1 < 5 ){
+    alert (" Reprovado " + aluno1)
+}
+aluno2 = prompt("Qual o nome do aluno 2 ?")
+notas2 = prompt(("Digite três notas separadas por virgula:"))
+notas2 = notas2.split(",")
+n1 =notas2[0]
+n2 =notas2[1]
+n3 =notas2[2]
+media2 = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3
+if ( media2 >= 5 ){
+    alert (" Aprovado " + aluno2)
+}
+else if ( media2 < 5 ){
+    alert (" Reprovado " + aluno2)
+}
+
+aluno3 = prompt("Qual o nome do aluno 3 ?")
+notas3 = prompt(("Digite três notas separadas por virgula:"))
+notas3 = notas3.split(",")
+n1 =notas3[0]
+n2 =notas3[1]
+n3 =notas3[2]
+media3 = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3
+if ( media3 >= 5 ){
+    alert ("Aprovado" + aluno3)
+}
+else if ( media1 < 5 ){
+    alert (" Reprovado " + aluno3)
+}
+aluno4 = prompt("Qual o nome do aluno 4 ?")
+notas4 = prompt(("Digite três notas separadas por virgula:"))
+notas4 = notas4.split(",")
+n1 =notas4[0]
+n2 =notas4[1]
+n3 =notas4[2]
+media4 = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3
+if ( media4 >= 5 ){
+    alert (" Aprovado " + aluno4)
+}
+else if ( media4  <  5 ){
+    alert (" Reprovado " + aluno4)
+}
+aluno5 = prompt("Qual o nome do aluno 5 ?")
+notas5 = prompt(("Digite três notas separadas por virgula:"))
+notas5 = notas5.split(",")
+n1 =notas5[0]
+n2 =notas5[1]
+n3 =notas5[2]
+media5 = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3
+if ( media5 >= 5 ){
+    alert (" Aprovado " + aluno5)
+}
+else if ( media5  <  5 ){
+    alert (" Reprovado " + aluno5)
+}
